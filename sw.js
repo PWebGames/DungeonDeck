22c280eddefcb409c932ab817671a82182d4d9f8
Bread <stonedbybread@gmail.com>
2025-12-18 18:30:00 -0800

    Force Cache reset for mobile users

--- sw.js
+++ sw.js
@@ -1,14 +1,33 @@
-const CACHE_NAME = "Balatro-cache-v1";
+const CACHE_NAME = "Balatro-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
   "./icons/512.png",
-  "./icons/192.png"
+  "./icons/192.png",
+  "./balatro.html"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((cacheNames) =>
+        Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        )
+      )
+    ])
   );
 });
 
